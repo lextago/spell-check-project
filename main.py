b589676d3d52557c95f76cc38a074082fd4cd014
lextago <alexetago@gmail.com>
2023-03-15 10:12:55 -0500

    3/15/23
    Co-authored-by: madisonrowe <madisonrowe@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import math, os, pickle
+from typing import Tuple, List, Dict
+
+class spell_check:
+    """"spell check class
+    
+    Variables:
+    
+        words_dict[Word, Frequency] - dictionary of correcty spelled words and their frequency
+        user_sentence - sentence sent as input (should be iterated thru)
+    
+    
+    """ 
+    """ this constructor initializes 
+    """
+    
+    def __init__(self):
+        self.words_dict: Dict[str, int] = {}
+        
+        if os.path.isfile(self.words.dat):
+            print("Data files found... teehee")
+            self.load_file()
+
+
+
 if __name__ == "__main__":
     print("Hello")
 
+# _/(o v o)/* oh i think that i found myself a cheerleader
+# she is always right there when i need her
